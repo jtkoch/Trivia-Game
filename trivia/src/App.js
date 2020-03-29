cead2248729ef906c6d39bd371316ddc2801a46b
Jensen Koch <jtkoch12@gmail.com>
2020-03-29 14:25:12 -0500

    finished app but I don't know why I am getting strange symbols in some of the answers

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { Route } from 'react-router-dom';
 import './App.scss';
+import axios from 'axios';
+import Header from './components/Header/Header';
 
-function App() {
-  return (
+const App = () => {
+  const [question, setQuestion] = useState({ results: [] });
+  const [answer,setAnswer] = useState();
+
+  const APIcall = () => {
+    axios
+      .get('https://opentdb.com/api.php?amount=1&difficulty=medium&type=boolean')
+      .then(res => {
+        setQuestion(res.data.results[0]);
+      })
+      .catch(err => console.log(err));
+  }
+
+  useEffect(() => {
+    APIcall();
+  }, [])
+
+  const answerChoice = (choice) => {
+    setAnswer(choice);
+
+    if (answer === question.correct_answer) {
+      console.log("correct");
+      alert("correct");
+      APIcall();
+    } else {
+      console.log("wrong");
+      alert("Better luck next time")
+      APIcall();
+    }
+  };
+
+  return(
     <div className="App">
-      <h1>Trivia Game</h1>
+      <Route path="/" component={Header} />
+      <div className="trivia">
+        <h1 className="intro">Choose Wisely</h1>
+        <div className="question-section">
+          <p className="category">{question.category}</p>
+          <h2 className="question">{question.question}</h2>
+        </div>
+        <div className="answer-section">
+          <button className="answer-button" onClick={() => answerChoice("True")}>True</button>
+          <button className="answer-button" onClick={() => answerChoice("False")}>False</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ trivia/src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return(
+    <div className="header">
+      <span className="t">T</span>
+      <span className="r">R</span>
+      <span className="i">I</span>
+      <span className="v">V</span>
+      <span className="i">I</span>
+      <span className="a">A</span>
+    </div>
+  );
+}
+
+export default Header;
